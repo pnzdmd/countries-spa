ea8277bb8ecc8543423a3293e032d550cffb13bf
Oleg Novikov <pnzdmd@yandex.ru>
2022-05-10 16:18:05 +0300

    Реализовать search

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import Controls from './components/Controls/Controls';
 import Header from './components/Header/Header';
+import Main from './components/Main/Main';
 
 function App() {
   return (
     <>
       <Header />
+      <Main>
+        <Controls />
+      </Main>
     </>
   );
 }
---
+++ src/components/Controls/Controls.jsx
@@ -0,0 +1,45 @@
+import { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import Search from '../Search/Search';
+import { CustomSelect } from '../CustomSelect/CustomSelect';
+
+const options = [
+  { value: 'Africa', label: 'Africa' },
+  { value: 'America', label: 'America' },
+  { value: 'Asia', label: 'Asia' },
+  { value: 'Europe', label: 'Europe' },
+  { value: 'Oceania', label: 'Oceania' },
+];
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+
+  @media (min-width: 767px) {
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+  }
+`;
+
+const Controls = () => {
+  const [search, setSearch] = useState('');
+  const [region, setRegion] = useState('');
+
+  return (
+    <Wrapper>
+      <Search search={search} setSearch={setSearch} />
+      <CustomSelect
+        options={options}
+        placeholder='Filter by Region'
+        isClearable
+        isSearchable={false}
+        value={region}
+        onChange={setRegion}
+      />
+    </Wrapper>
+  );
+};
+
+export default Controls;
