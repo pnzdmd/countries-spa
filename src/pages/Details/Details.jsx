2ab7e0860697025e61a56b4d8192a3e0f9659dda
Oleg Novikov <pnzdmd@yandex.ru>
2022-05-11 10:31:46 +0300

    Создать карточку с информацией

--- src/pages/Details/Details.jsx
+++ src/pages/Details/Details.jsx
@@ -1,13 +1,29 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
+import { IoArrowBack } from 'react-icons/io5';
+import { searchByCountry } from '../../config';
+import { Button } from '../../components/Button/Button';
+import Info from '../../components/Info/Info';
 
 const Details = () => {
   const { name } = useParams();
   const navigate = useNavigate();
+  const [country, setCountry] = useState(null);
+
+  //console.log(country);
+
+  useEffect(() => {
+    axios.get(searchByCountry(name)).then(({ data }) => setCountry(data[0]));
+  }, [name]);
 
   return (
     <div>
-      Details
-      {name}
+      <Button onClick={() => navigate(-1)}>
+        <IoArrowBack />
+        Назад
+      </Button>
+      {country && <Info navigate={navigate} {...country} />}
     </div>
   );
 };
